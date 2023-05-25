cb5745d267295c684bfbf1b4539b819938986b9a
Rence Salaveria <clarencerheysalaveria@plv.edu.ph>
2023-05-26 00:10:09 +0800

    Fix sample data

--- src/stores/global.store.js
+++ src/stores/global.store.js
@@ -3,11 +3,11 @@ export const dataRecord = [
     'id': 1,
     'motorLoad': '100',
     'location': 'Valenzuela',
-    'voltage': '220',
-    'current': '10',
-    'resistance': '15',
-    'groundVoltage': '20',
-    'power': '30',
-    'faultType': 'Line B open'
+    'voltage': '230',
+    'current': '0.8',
+    'resistance': '287.5',
+    'groundVoltage': '115',
+    'power': '184',
+    'faultType': 'No Fault'
   }
 ]
